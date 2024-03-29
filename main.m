bbeddb534ecd62599929f7f2b90a805c335ac9b9
Vincenzo Pio Florio <vincenzopioflorio@gmail.com>
2024-03-29 21:01:18 +0100

    Hotfixes
    Main needs to be fixed based on kmeans method

--- main.m
+++ main.m
@@ -6,4 +6,13 @@ hcube = imread(image_path);
 [image,components] = PCA(hcube,3);
 
 % Index Calculation
-[ndvi, msavi, tsavi] = compute_indices(hcube);
+%[ndvi, msavi, tsavi] = compute_indices(hcube);
+
+% Trying classification
+classified_image = kmeans(image, 3);
+
+figure;
+imshow(classified_image, []);
+colormap(jet(3));
+colorbar;
+title('K-means++ clustering results');
