72d830b3a439533722fc6fcd88526f5a5f966f65
Vincenzo Pio Florio <vincenzopioflorio@gmail.com>
2024-04-06 19:37:41 +0200

    Added results

--- main.m
+++ main.m
@@ -13,3 +13,15 @@ hcube = imread(image_path);
 classified_image = kmeans_calculation(image, 3);
 
 corrected_image = apply_radiometric_correction(hcube, atmospheric_correction, wavelength);
+
+function [corrected_hcube] = apply_radiometric_correction(hcube, atmospheric_correction, wavelength)
+
+    % Supponiamo che 'hcube' sia un ipercubo 3D dove la terza dimensione corrisponde alle lunghezze d'onda
+    % e 'atmospheric_correction' sia un array 1D delle stesse dimensioni dell'array 'wavelength'.
+
+    corrected_hcube = hcube;
+
+    for i = 1:length(wavelength)
+        corrected_hcube(:,:,i) = radiometric_correction(hcube(:,:,i), atmospheric_correction(i));
+    end
+end
--- radiometric_correction.m
+++ radiometric_correction.m
@@ -10,15 +10,3 @@ function [corrected_img] = radiometric_correction(img, atmospheric_correction)
     corrected_img(corrected_img < 0) = 0;
     corrected_img(corrected_img > 1) = 1;
 end
-
-function [corrected_hcube] = apply_radiometric_correction(hcube, atmospheric_correction, wavelength)
-
-    % Supponiamo che 'hcube' sia un ipercubo 3D dove la terza dimensione corrisponde alle lunghezze d'onda
-    % e 'atmospheric_correction' sia un array 1D delle stesse dimensioni dell'array 'wavelength'.
-
-    corrected_hcube = hcube;
-
-    for i = 1:length(wavelength)
-        corrected_hcube(:,:,i) = radiometric_correction(hcube(:,:,i), atmospheric_correction(i));
-    end
-end
