e4f51b552154e099bbb65276b95bca340fe6d5fd
Vincenzo Pio Florio <vincenzopioflorio@gmail.com>
2024-04-09 21:32:19 +0200

    Create kmedoidplots.m

---
+++ kmedoidplots.m
@@ -0,0 +1,34 @@
+clc
+clear
+
+% Loading image
+image_path = 'CROP1_47.tiff';
+hcube = imread(image_path);
+
+% Calculating PCA
+[image,components] = PCA(hcube,5);
+
+% Create the directory if it doesn't exist
+if ~exist('./Results/kmedoids', 'dir')
+    mkdir('./Results/kmedoids');
+end
+
+% Reshaping image
+pixels = reshape(image, [], size(image, 3));
+
+% Set options
+opts = statset('Display','final','MaxIter',100);
+
+% Define the range of cluster numbers
+cluster_range = 4:7;
+
+for num_clusters = cluster_range
+    % Run k-medoids
+    [idx,centers] = kmedoids(pixels,num_clusters,'Options',opts);
+
+    % Obtaining image's sizes
+    [rows, cols, ~] = size(image);
+
+    % Save the figure
+    imwrite(reshape(idx, [rows, cols]), jet(num_clusters), ['./Results/kmedoids/kmedoids_', num2str(num_clusters), '.png']);
+end
