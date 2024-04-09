a9f54118e85dd36ad460d6a19a9e8c2190a8a135
Vincenzo Pio Florio <vincenzopioflorio@gmail.com>
2024-04-09 21:20:35 +0200

    Commented figures

--- PCA.m
+++ PCA.m
@@ -3,12 +3,12 @@ function [img,reduced_image] = PCA(hcube,principal_components)
 reduced_image = hyperpca(hcube,principal_components);
 
 img = squeeze(reduced_image(:,:,1)); % Extracting the first image
-imshow(img,[]); % Visualize image
+%imshow(img,[]); % Visualize image
 
 rescalePC = rescale(reduced_image,0,1);
 
-figure
-montage(rescalePC,'BorderSize',[10 10],'Size',[1 3]);
-title('Principal Component Bands of Data Cube')
+% figure
+% montage(rescalePC,'BorderSize',[10 10],'Size',[1 3]);
+% title('Principal Component Bands of Data Cube')
 end
 
