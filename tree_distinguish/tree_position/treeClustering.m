c2851ad6f078c3c4a560f5e75c6c4b8bbcd7a62b
Vincenzo Pio Florio <vincenzopioflorio@gmail.com>
2024-04-10 12:39:18 +0200

    Added kmedoids for trees' images

--- tree_distinguish/tree_position/treeClustering.m
+++ tree_distinguish/tree_position/treeClustering.m
@@ -1,9 +1,9 @@
 %We will now use the masked hypercube generated from the tree filtering to cluster the trees in different categories (unsuperivised algorithms)
 % I = geotiffinfo('CROP1_47.tiff');
 treecube=hypercube("tree_distinguish\source_cropping\crop_trees.dat","tree_distinguish\source_cropping\crop_trees.hdr");
-cluster_range = 4:6;
 image=treecube.DataCube;
-num_clusters = length(cluster_range);
+starting_cluster = 4;
+end_cluster = 7;
 
 % Create the directory if it doesn't exist
 if ~exist('./Results/kmedoids', 'dir')
@@ -13,17 +13,17 @@ pixels = reshape(image, [], size(image,3));
 opts = statset('Display','final','MaxIter',100);
 
 % Loop over the number of clusters
-for i = 1:num_clusters
+for i = starting_cluster:end_cluster
     % Run imsegkmeans
-    %[L,centers] = imsegkmeans(treecube.DataCube, cluster_range(i));
+    %[L,centers] = imsegkmeans(treecube.DataCube, i);
     %imwrite(L, jet(cluster_range(i)), ['./Results/imgkmeansResults/k_meanstrees', num2str(cluster_range(i)), '.png']);
 
     %run kmedoids
-    [idx,centers] = kmedoids(pixels,num_clusters,'Options',opts);
+    [idx,centers] = kmedoids(pixels,i,'Options',opts);
     
     % Obtaining image's sizes
     [rows, cols, ~] = size(image);
 
     % Save the figure
-    imwrite(reshape(idx, [rows, cols]), jet(num_clusters), ['./Results/kmedoids/kmedoids_trees', num2str(num_clusters), '.png']);
+    imwrite(reshape(idx, [rows, cols]), jet(i), ['./Results/kmedoids/kmedoids_trees', num2str(i), '.png']);
 end
