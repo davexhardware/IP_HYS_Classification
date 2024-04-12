b40c242bfaa2e9e3c38240c717e539b7c3a71ee0
Vincenzo Pio Florio <vincenzopioflorio@gmail.com>
2024-04-12 09:26:02 +0200

    Added time evaluation

--- classification_scripts/kmeanspp.m
+++ classification_scripts/kmeanspp.m
@@ -22,15 +22,23 @@ pixels = reshape(image, [], size(image, 3));
 % Set options
 opts = statset('Display','final','MaxIter',100000, 'TolFun',1e-6);
 
+% UNCOMMENT THIS IF YOU NEED KMEANS
+%opts = statset('Display','final','MaxIter',10000);
+
 % Loop over the number of clusters
 for num_clusters = cluster_range
+    tic
     % Run k-means++ with 'plus' option
     [idx, centers] = kmeans(pixels, num_clusters, 'Start', 'plus', 'Options', opts);
+
+    %Run k-means with 'plus' option UNCOMMENT THIS IF YOU NEED KMEANS
+    %[idx, centers] = kmeans(pixels, num_clusters, 'Options', opts); 
 
     % Obtaining image's sizes
     [rows, cols, ~] = size(image);
 
+    toc
+
     % Save the figure
     imwrite(reshape(idx, [rows, cols]), jet(num_clusters), ['./Results/kmeans++/kmeans_', num2str(num_clusters), '.png']);
-end
-    
+end   
