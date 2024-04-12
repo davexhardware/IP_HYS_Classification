f596658544ec733260ab4d6b552a9f004dae023f
Vincenzo Pio Florio <vincenzopioflorio@gmail.com>
2024-04-12 10:14:08 +0200

    Added kmedoids images and hotfixes

--- classification_scripts/kmedoidplots.m
+++ classification_scripts/kmedoidplots.m
@@ -3,10 +3,10 @@ clear
 
 % Loading image
 image_path = 'CROP1_47.tiff';
-hcube = imread(image_path);
+image = imread(image_path);
 
 % Calculating PCA
-[image,components] = PCA(hcube,5);
+%[image,components] = PCA(hcube,5);
 
 % Create the directory if it doesn't exist
 if ~exist('./Results/kmedoids', 'dir')
@@ -23,11 +23,16 @@ opts = statset('Display','final','MaxIter',100);
 cluster_range = 4:7;
 
 for num_clusters = cluster_range
+
+    tic
+
     % Run k-medoids
     [idx,centers] = kmedoids(pixels,num_clusters,'Options',opts);
 
     % Obtaining image's sizes
     [rows, cols, ~] = size(image);
+
+    toc
 
     % Save the figure
     imwrite(reshape(idx, [rows, cols]), jet(num_clusters), ['./Results/kmedoids/kmedoids_', num2str(num_clusters), '.png']);
