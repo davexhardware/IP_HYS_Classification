7bd503256eb3ca4b7ffc94a4b5b644c7a7ca5010
Vincenzo Pio Florio <vincenzopioflorio@gmail.com>
2024-04-12 22:34:03 +0200

    Implemented GMM

--- tree_distinguish/tree_classification/treeClassification.m
+++ tree_distinguish/tree_classification/treeClassification.m
@@ -2,24 +2,54 @@ clc
 clear
 
 % Caricamento dell'immagine
-hcube = imread('Seg_Crop1.tif');
-load("testGT.mat")
+hcube = hypercube('crop_trees.dat', 'crop_trees.hdr');
 load("wavelengths.mat")
 load("labeled_tree_coordinates.mat")
 
-score = zeros(size(hcube.DataCube,1),size(hcube.DataCube,2),numEndmembers);
-for i = 1:numEndmembers
-    score(:,:,i) = sam(hcube,endmembers(:,i));
-end
+labels = convxy(:,3);
 
-labels = convxy(3,:);
+coords = convxy(:,1:2);
 
-% Addestramento della Random Forest
-numTrees = 500; % Aumenta il numero di alberi
-RFmodel = TreeBagger(numTrees, pixelsTrain, labelsTrain, 'Method', 'classification', 'OOBPrediction', 'on');
+[rows, columns, bands] = size(hcube.DataCube);
 
-% Predici le classi sui dati di test
-[idx_rf,scores] = predict(RFmodel, pixelsTest);
+% Reshaping of image
+image = reshape(hcube.DataCube, [rows*columns, bands]);
 
-% Converti l'array di celle delle previsioni in matrice
-idx_rf = cellfun(@str2double, idx_rf);
+% Number of clusters
+k = 2;
+
+% Non classificare i valori 0
+image_no_zero = image(any(image~=0,2),:);
+
+gmm = fitgmdist(image_no_zero, k);
+
+gmm_labels = cluster(gmm, image_no_zero);
+
+% Creazione di un'immagine vuota
+clustered_image = zeros(rows*columns,1);
+
+% Inserimento delle etichette nel luogo appropriato
+clustered_image(any(image~=0,2)) = gmm_labels;
+
+clustered_image = reshape(clustered_image, [rows, columns]);
+
+% Creazione del plot
+figure;
+imshow(clustered_image,[]);
+colormap("jet");
+colorbar;
+title('GMM');
+
+% hold on;
+% 
+% markerSize = 10;
+% 
+% for i = 1:length(labels)
+%     if labels(i) == 0
+%         plot(coords(i,1), coords(i,2), 'go', 'MarkerSize', markerSize);
+%     else
+%         plot(coords(i,1), coords(i,2), 'wx', 'MarkerSize', markerSize);
+%     end
+% end
+% 
+% hold off; 
