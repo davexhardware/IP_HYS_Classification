ee279c2eaa3626517a028e782ee382bdef4a36c0
Vincenzo Pio Florio <vincenzopioflorio@gmail.com>
2024-04-12 14:12:32 +0200

    Update imagekmeans.m

--- classification_scripts/imagekmeans.m
+++ classification_scripts/imagekmeans.m
@@ -3,7 +3,10 @@ clear
 
 % Loading image
 image_path = 'CROP1_47.tiff';
-hcube = imread(image_path);
+image = imread(image_path);
+
+% Calculating PCA
+[image,components] = PCA(image,5);
 
 % Define the range of clusters
 cluster_range = 4:7;
@@ -16,9 +19,14 @@ end
 
 % Loop over the number of clusters
 for i = 1:num_clusters
+
+    tic
+
     % Run imsegkmeans
-    [L,centers] = imsegkmeans(hcube, cluster_range(i));
+    [L,centers] = imsegkmeans(image, cluster_range(i));
+
+    toc
     
     % Save the segmented image as an image
-    imwrite(L, jet(cluster_range(i)), ['./Results/imgkmeansResults/k', num2str(cluster_range(i)), '.png']);
+    imwrite(L, jet(cluster_range(i)), ['./Results/imgkmeansResults/PCAk', num2str(cluster_range(i)), '.png']);
 end
