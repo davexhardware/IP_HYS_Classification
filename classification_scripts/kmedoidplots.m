3c04ffb2849d44386e8619b85082ea8f33d890c6
Vincenzo Pio Florio <vincenzopioflorio@gmail.com>
2024-04-18 12:03:02 +0200

    Added mask on kmedoids

--- classification_scripts/kmedoidplots.m
+++ classification_scripts/kmedoidplots.m
@@ -6,7 +6,7 @@ image_path = 'CROP1_47.tiff';
 image = imread(image_path);
 
 % Calculating PCA
-[image,components] = PCA(image,5);
+%[image,components] = PCA(image,5);
 
 % Create the directory if it doesn't exist
 if ~exist('./Results/kmedoids', 'dir')
@@ -35,5 +35,15 @@ for num_clusters = cluster_range
     toc
 
     % Save the figure
-    imwrite(reshape(idx, [rows, cols]), jet(num_clusters), ['./Results/kmedoids/kmedoids_', num2str(num_clusters), '.png']);
+    %imwrite(reshape(idx, [rows, cols]), jet(num_clusters), ['./Results/kmedoids/kmedoids_', num2str(num_clusters), '.png']);
 end
+%% 
+
+L = reshape(idx, [rows, cols]);
+
+cmp = [ 1 0 0; 0 0 0; 0 0 0; 0 0 0; 0 0 0; 0 0 0; 0 0 0;];
+figure; 
+imagesc( L );
+colormap( cmp ); 
+axis off;
+axis image;
