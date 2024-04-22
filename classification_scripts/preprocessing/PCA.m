17bcba9ac875685b4d0d6a987a31183354e11301
Vincenzo Pio Florio <vincenzopioflorio@gmail.com>
2024-04-22 12:32:59 +0200

    Added variance evaluations

--- classification_scripts/preprocessing/PCA.m
+++ classification_scripts/preprocessing/PCA.m
@@ -1,11 +1,15 @@
 function [img,reduced_image] = PCA(hcube,principal_components)
 
-reduced_image = hyperpca(hcube,principal_components);
+[reduced_image, coeff, varianceRetained] = hyperpca(hcube,principal_components);
 
 img = squeeze(reduced_image(:,:,1)); % Extracting the first image
 %imshow(img,[]); % Visualize image
 
 rescalePC = rescale(reduced_image,0,1);
+
+varianceRetained = transpose(varianceRetained);
+
+disp(varianceRetained)
 
 % figure
 % montage(rescalePC,'BorderSize',[10 10],'Size',[1 3]);
